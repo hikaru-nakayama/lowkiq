5b12d3146fa987a344eb8d626ef2ef535ed45015
hikaru-nakayama <hikaru-nakayama@c-fo.com>
2024-01-03 11:55:30 +0900

    :recycle: Change to accept class

--- example/test.rb
+++ example/test.rb
@@ -2,4 +2,11 @@ require_relative '../lib/lowkiq'
 require 'pry'
 
 Lowkiq.redis = 'redis://localhost:6379'
-Lowkiq.push('archive', 'hello world')
+Lowkiq.enqueue(CalcStatementJob, 1)
+
+class CalcStatementJob
+    @queue = :payroll_statement_test
+    def self.perform(id)
+        puts "実行！！#{id}"
+    end
+end
--- lib/lowkiq/job.rb
+++ lib/lowkiq/job.rb
@@ -5,13 +5,27 @@ module Lowkiq
         @payload = payload
     end
 
-    def self.create(queue, klass)
-      Lowkiq.push(queue, klass.to_s)
+    def self.create(queue, klass, *args)
+      Lowkiq.push(queue, :class => klass.to_s, :args => args)
     end
 
     def self.reserve(queue)
         return unless payload = Lowkiq.pop(queue)
         new(queue, payload)
+    end
+
+    def perform
+        job = payload_class
+        args = @payload['args']
+        job.perform(*args)
+    end
+
+    def payload_class
+        @payload_class ||= constantize(@payload['class'])
+    end
+
+    def constantize(camel_cased_word)
+        Lowkiq.constantize(camel_cased_word)
     end
   end
 end
